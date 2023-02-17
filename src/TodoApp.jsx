fe0c7232915ed08470e8e0b8432590048fa2bbd6
Alejandro Perdomo <diegoleandro-29@hotmail.com>
2023-02-17 11:55:00 -0500

    RTK Query

--- src/TodoApp.jsx
+++ src/TodoApp.jsx
@@ -1,15 +1,58 @@
+import { useState } from "react";
+import { useGetTodoQuery, useGetTodosQuery } from "./store/apis"
+
 export const TodoApp = () => {
-  return (
-    <>
+
+  const [todoId, setTodoId] = useState(1);
+  // const { data: todos = [], isLoading } = useGetTodosQuery();
+  const { data: todo, isLoading } = useGetTodoQuery(todoId);
+
+  const nextTodo = () => {
+    setTodoId(todoId + 1);
+  }
+
+  const prevTodo = () => {
+    if (todoId === 1) return;
+    setTodoId(todoId - 1);
+  }
+
+  // return (
+  //   <>
+  //       <h1>Todos - RTK Query</h1>
+  //       <hr/>
+  //       <h4>isLoading: {isLoading ? 'True' : 'False'}</h4>
+
+  //       <pre>...</pre>
+
+  //       <ul>
+  //         {todos.map( todo => (
+  //           <li key={todo.id}>
+  //             <strong> {todo.completed ? 'Done' : 'Pending'} </strong>
+  //             {todo.title}
+  //           </li>
+  //         ))}
+  //       </ul>
+
+  //       <button>
+  //           Next Todo
+  //       </button>
+  //   </>
+  // )
+
+    return (
+      <>
         <h1>Todos - RTK Query</h1>
         <hr/>
-        <h4>isLoading...</h4>
+        <h4>isLoading: {isLoading ? 'True' : 'False'}</h4>
 
-        <pre>...</pre>
+        <pre>{JSON.stringify(todo)}</pre>
 
-        <button>
-            Next Todo
+        <button onClick={prevTodo}>
+          Prev Todo
         </button>
-    </>
+        <button onClick={nextTodo}>
+          Next Todo
+        </button>
+      </>
   )
 }
